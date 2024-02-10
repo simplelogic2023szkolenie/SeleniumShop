39ea9ad0605f556739d512005810d79de452c4b4
Mateusz Tadla <mat@scalepoint.com>
2024-02-10 12:37:05 +0100

    update

--- src/main/java/pages/base/BasePage.java
+++ src/main/java/pages/base/BasePage.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -13,29 +12,31 @@ public class BasePage {
     public Actions actions;
     public WebDriver driver;
     public WebDriverWait defaultWait;
+
     public BasePage(WebDriver driver) {
-        PageFactory.initElements(driver,this);
+        PageFactory.initElements(driver, this);
         initDriver(driver);
     }
 
-    private void initDriver(WebDriver driver){
+    private void initDriver(WebDriver driver) {
         this.driver = driver;
         actions = new Actions(driver);
         defaultWait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
-    public void sendKeys(WebElement element, String textToSet){
-        System.out.println("Typing: "+ textToSet);
+    public void sendKeys(WebElement element, String textToSet) {
+        System.out.println("Typing: " + textToSet);
         element.sendKeys(textToSet);
     }
 
-    public void sendKeysAndClear(WebElement element, String textToSet){
+    public void sendKeysAndClear(WebElement element, String textToSet) {
+        System.out.println("Clearing input");
         element.clear();
         sendKeys(element, textToSet);
     }
 
-    public void click(WebElement element){
-        System.out.println("clicking on: " + element.getText().replace("\r\n"," "));
+    public void click(WebElement element) {
+        System.out.println("clicking on: " + element.getText().replace("\r\n", " "));
         element.click();
     }
 }
