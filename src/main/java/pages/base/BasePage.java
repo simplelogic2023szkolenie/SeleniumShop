5909d6cb270e7aa845c5163d12e55cef30e86ec2
Mateusz Tadla <mat@scalepoint.com>
2024-02-10 12:10:39 +0100

    update

--- src/main/java/pages/base/BasePage.java
+++ src/main/java/pages/base/BasePage.java
@@ -1,10 +1,26 @@
 package pages.base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class BasePage {
+    public Actions actions;
+    public WebDriver driver;
+    public WebDriverWait defaultWait;
     public BasePage(WebDriver driver) {
         PageFactory.initElements(driver,this);
+        initDriver(driver);
     }
+
+    private void initDriver(WebDriver driver){
+        this.driver = driver;
+        actions = new Actions(driver);
+        defaultWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+
 }
