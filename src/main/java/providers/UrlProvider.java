c1ccb4542023f86dabce9e0189acc0750658a702
Mateusz Tadla <mat@scalepoint.com>
2024-02-11 15:05:26 +0100

    update

--- src/main/java/providers/UrlProvider.java
+++ src/main/java/providers/UrlProvider.java
@@ -1,9 +1,11 @@
 package providers;
 
-public class UrlProvider {
-    private static String baseUrl="http://146.59.32.4/index.php";
+import configuration.Config;
 
+public class UrlProvider {
+    private static String baseUrl = Config.getAppUrl();
     public static String cart = baseUrl + "?controller=cart&action=show";
+    public static String login = baseUrl + "?controller=authentication&back=my-account";
     public static String homePage = baseUrl;
 
     // + budowanie innych URLi które byśmy wykorzystywali w teście
