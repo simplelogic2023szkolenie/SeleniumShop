f8d306747d13c2d79ead3caaad474ca46e3af14f
Mateusz Tadla <mat@scalepoint.com>
2024-02-11 10:32:15 +0100

    update

--- src/main/java/pages/products/ProductMiniatureComponent.java
+++ src/main/java/pages/products/ProductMiniatureComponent.java
@@ -1,10 +1,27 @@
 package pages.products;
 
-public class ProductMiniatureComponent {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import pages.base.BasePage;
 
+public class ProductMiniatureComponent extends BasePage {
+    public ProductMiniatureComponent(WebElement parent, WebDriver driver) {
+        super(parent, driver);
+    }
+    @FindBy(css = ".product-title")
+    private WebElement name;
+    public String getName(){
+        return name.getText();
+    }
     // konstruktor z BP uzuwający parent oraz driver
 
     // webelement + metody zwaracjące: discount, price, priceBeforeDiscount, name
 
     // do tego też metoda open() która otwera dany produt
+
+    public ProductDetailsPage open(){
+        click(name);
+        return new ProductDetailsPage(driver);
+    }
 }
